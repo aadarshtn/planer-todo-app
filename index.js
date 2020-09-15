fdfd95ceb679dc4336387d90de8658ce1375d001
aadarshtn <aadarshtn@gmail.com>
2020-09-16 02:17:33 +0530

    Define Directory Relations; Install Initialise View Engine - EJS;

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// MiddleWares
+app.use('/', require('./routes'));     //Setting Up Initial Routes
+app.set('view engine', 'ejs');         //Setting Up View Engine
+app.set('views', './views');           //Setting Up Views Directory
 
 
 app.listen(port, function(err){
