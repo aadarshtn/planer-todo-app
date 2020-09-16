59fa411c425269241e52cd6b6e1db21eb6c07289
aadarshtn <aadarshtn@gmail.com>
2020-09-16 23:03:27 +0530

    Include Express Static ; Include Static Directories ; Structure And Design App

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 8000;
 
 // MiddleWares
 app.use('/', require('./routes'));     //Setting Up Initial Routes
+app.use(express.static('assets'));     //Setting Up Static Directories
+
 app.set('view engine', 'ejs');         //Setting Up View Engine
 app.set('views', './views');           //Setting Up Views Directory
 
